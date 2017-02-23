e326edefae00e3e3aa4ef3f2d1073f60f3fb5feb
张猛 <921333285@qq.com>
2017-02-23 13:39:18 +0800

    修改错别字

--- src/meng/spring/test6/autowiring/AutoWiringService.java
+++ src/meng/spring/test6/autowiring/AutoWiringService.java
@@ -4,7 +4,7 @@ public class AutoWiringService {
 
 	private AutoWiringDAO autoWiringDAO;
 
-	/* default-autowire="byName" 和 default-autowire="byType"  时会使用到设置注入 */
+	/* default-autowire="byName" 和 default-autowire="byType"  时会使用到设值注入 */
 	public void setAutoWiringDAO(AutoWiringDAO autoWiringDAO) {
 		System.out.println("byName和byType");
 		this.autoWiringDAO = autoWiringDAO;
